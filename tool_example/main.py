26e53b890cff067575039a4e86954054c1d297dd
Chad Skeeters <chad.skeeters@gmail.com>
2026-07-18 17:07:07 -0500

    updates main

--- tool_example/main.py
+++ tool_example/main.py
@@ -1,12 +1,4 @@
 import sys
 
 def main() -> None:
-    """Entry point for ``my-command``.
-
-    If arguments are provided they are printed; otherwise a default string is
-    output. This satisfies the requirement of printing one line of any text.
-    """
-    if len(sys.argv) > 1:
-        print(" ".join(sys.argv[1:]))
-    else:
-        print("tool-example output")
+    print("tool-example output 2")
